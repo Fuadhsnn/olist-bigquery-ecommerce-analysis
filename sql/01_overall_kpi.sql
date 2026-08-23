cab795da0d19e14f9d03566affb9ceaad337142b
Fuad Hasan <hasanyudha27@gmail.com>
2026-08-23 14:20:32 +0700

    Refactor KPI query to include order items and customers

--- sql/01_overall_kpi.sql
+++ sql/01_overall_kpi.sql
@@ -1,12 +1,11 @@
 SELECT
     COUNT(DISTINCT o.order_id) AS total_orders,
-    COUNT(DISTINCT o.customer_id) AS total_customers,
-    ROUND(SUM(p.payment_value), 2) AS total_revenue,
-    ROUND(
-        SUM(p.payment_value) / COUNT(DISTINCT o.order_id),
-        2
-    ) AS average_order_value
+    COUNT(DISTINCT c.customer_unique_id) AS total_customers,
+    ROUND(SUM(oi.price), 2) AS total_revenue,
+    ROUND(SUM(oi.price) / COUNT(DISTINCT o.order_id), 2) AS average_order_value
 FROM `data-analyst-portfolio-505708.ecommerce_analysis.orders` o
-JOIN `data-analyst-portfolio-505708.ecommerce_analysis.order_payments` p
-    USING (order_id)
+JOIN `data-analyst-portfolio-505708.ecommerce_analysis.customers` c
+    ON o.customer_id = c.customer_id
+JOIN `data-analyst-portfolio-505708.ecommerce_analysis.order_items` oi
+    ON o.order_id = oi.order_id
 WHERE o.order_status = 'delivered';
